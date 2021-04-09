6fd95111958ee27c5cddc3af04444b9a185eb4bd
sailorj94 <jackniall9@gmail.com>
2021-04-09 13:43:31 +0100

    Room cash variable added and testing OK

--- codeclan_caraoke/src/room.py
+++ codeclan_caraoke/src/room.py
@@ -2,12 +2,17 @@ class Room:
     
     # Each room will be constructed as below with a descriptive room name, variable capacity, variable cost per person and a unique playlist.
 
-    def __init__(self, roomname, capacity, costpp, playlist, guestlist):
+    def __init__(self, roomname, capacity, costpp, playlist, guestlist, roomtakings):
         self.roomname = roomname
         self.capacity = capacity
         self.costpp = costpp
         self.playlist = playlist
         self.guestlist = guestlist
+        self.roomtakings = roomtakings
 
     def add_guest_to_room(self, guest):
-        pass
+        if (len(self.guestlist) < self.capacity) and guest.cash >= self.costpp:
+            self.guestlist.append(guest)
+            guest.cash -= self.costpp
+            self.roomtakings += self.costpp
+            
--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -10,9 +10,9 @@ class TestRoom(unittest.TestCase):
         self.song2 = Song("Number of the Beast", "Iron Maiden")
         self.guest1 = Guest("Dave", 20)
         self.guest2 = Guest("Catherine", 25)
-        self.guestlist = [self.guest1, self.guest2]
+        self.guestlist = []
         self.playlist = [self.song1, self.song2]
-        self.rockroom = Room("Rock Room", 6, 10, self.playlist, self.guestlist)
+        self.rockroom = Room("Rock Room", 6, 10, self.playlist, self.guestlist, 0)
 
     def test_room_has_roomname(self):
         self.assertEqual("Rock Room", self.rockroom.roomname)
@@ -28,5 +28,13 @@ class TestRoom(unittest.TestCase):
     def test_room_has_playlist(self):
         self.assertEqual(self.playlist, self.rockroom.playlist)
 
-    def test_room_has_guests_in(self):
-        self.assertEqual([self.guest1, self.guest2], self.rockroom.guestlist)
+    def test_room_has_guestlist(self):
+        self.assertEqual([], self.rockroom.guestlist)
+
+    def test_add_guest_to_room(self):
+        self.rockroom.add_guest_to_room(self.guest1)
+        self.assertEqual([self.guest1], self.rockroom.guestlist)
+
+    def test_room_takings_increase(self):
+        self.rockroom.add_guest_to_room(self.guest1)
+        self.assertEqual(10, self.rockroom.roomtakings)
