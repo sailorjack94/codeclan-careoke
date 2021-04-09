9bee71eac7326bda9ec1ed40d77061e8f8a2a7c9
sailorj94 <jackniall9@gmail.com>
2021-04-09 13:14:52 +0100

    Room has name test written and passing

--- codeclan_caraoke/tests/room_test.py
+++ codeclan_caraoke/tests/room_test.py
@@ -5,8 +5,10 @@ from src.song import *
 class TestRoom(unittest.TestCase):
     
     def setUp(self):
-        self.song1 = Song(self)
-        self.song2 = Song(self)
+        self.song1 = Song("Ace of Spades", "Motorhead")
+        self.song2 = Song("Number of the Beast", "Iron Maiden")
         self.playlist = [self.song1, self.song2]
-        self.roomrock = Room(self, "Rock Room", 6, 10, self.playlist)
+        self.roomrock = Room("Rock Room", 6, 10, self.playlist)
 
+    def test_room_has_roomname(self):
+        self.assertEqual("Rock Room", self.roomrock.roomname)
