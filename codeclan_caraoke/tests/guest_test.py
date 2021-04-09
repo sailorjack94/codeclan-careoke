9f8fe99ddd03bfdbf7571fcc780d36c998fb53e6
sailorj94 <jackniall9@gmail.com>
2021-04-09 13:09:18 +0100

    Guest class and test passing

--- codeclan_caraoke/tests/guest_test.py
+++ codeclan_caraoke/tests/guest_test.py
@@ -2,4 +2,8 @@ import unittest
 from src.guest import *
 
 class TestGuest(unittest.TestCase):
-    pass
+    def setUp(self):
+        self.guest = Guest("Niall")
+
+    def test_guest_has_name(self):
+        self.assertEqual("Niall", self.guest.name)
